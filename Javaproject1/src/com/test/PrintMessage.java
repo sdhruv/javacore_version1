eae88c82d92b97d013d3ef20e03a8e143c0ee041
sdhruv <dhruvshardul@gmail.com>
2016-01-07 19:42:37 -0600

    Added Hello
    Added Hello

---
+++ Javaproject1/src/com/test/PrintMessage.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class PrintMessage {
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello");
+		
+	}
+
+}
