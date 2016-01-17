bcf9c5462a265268efbc2d75602c7dfa0d4cb2d2
sdhruv <dhruvshardul@gmail.com>
2016-01-17 16:38:30 -0600

    Added Math Operation with Return Value and Parameters
    Added Math Operation with Return Value and Parameters

---
+++ ArithmaticOperations/src/com/digitek/mathoperations/WithReturnTypeAndAcceptParameters.java
@@ -0,0 +1,31 @@
+package com.digitek.mathoperations;
+
+public class WithReturnTypeAndAcceptParameters {
+
+	
+	public static int addNumbersWithReturnTypeAndAcceptParameters(int a, int b){
+		
+		int c = a+b;
+		System.out.println("result from addition with return type/accepts parameters:"+ c);
+		
+		return c;
+			
+	}
+	
+	public static void main(String args[]) {
+		
+		int result1 = addNumbersWithReturnTypeAndAcceptParameters(10,20);
+		int result2 = addNumbersWithReturnTypeAndAcceptParameters(50,20);
+		int result3 = addNumbersWithReturnTypeAndAcceptParameters(80,40);
+					
+		
+	}
+	
+
+			
+	}
+	
+	
+	
+	
+	
