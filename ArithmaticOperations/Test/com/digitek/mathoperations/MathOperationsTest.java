59d51239974fd8e4335f62321efe854e54b41c30
sdhruv <dhruvshardul@gmail.com>
2016-01-24 15:55:58 -0600

    Additions
    Additions

---
+++ ArithmaticOperations/Test/com/digitek/mathoperations/MathOperationsTest.java
@@ -0,0 +1,21 @@
+package com.digitek.mathoperations;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MathOperationsTest {
+
+	@Test
+	public void subtractNumbersTest() {
+		//Creating a Java Object
+		MathOperationsTest mo = new MathOperationsTest();
+	    void Result = mo.subtractNumbers(40,20)
+		Assert.assertEquals(100, actualResult);
+					
+		
+	}
+	
+	
+	
+	
+}
--- ArithmaticOperations/src/com/digitek/mathoperations/WithReturnTypeAndAcceptParameters.java
+++ ArithmaticOperations/src/com/digitek/mathoperations/WithReturnTypeAndAcceptParameters.java
@@ -12,12 +12,38 @@ public class WithReturnTypeAndAcceptParameters {
 			
 	}
 	
+	
+	 public static int dividenumbers(int numerator, int denominator) {
+   	  
+   	  int result = numerator/denominator;
+   	
+   	  System.out.println("result from division:"+ result);
+   	  return result;
+   
+	 }
+   
+	 public static double dividenumbers(int numerator, double denominator) {
+	   	  
+	   	  double result = numerator/denominator;
+	   	
+	   	  System.out.println("result from division:"+ result);
+	   	  return result;
+	   
+		 }
+	
+	
+	
 	public static void main(String args[]) {
 		
 		int result1 = addNumbersWithReturnTypeAndAcceptParameters(10,20);
 		int result2 = addNumbersWithReturnTypeAndAcceptParameters(50,20);
 		int result3 = addNumbersWithReturnTypeAndAcceptParameters(80,40);
-					
+		dividenumbers(5,2);
+		dividenumbers(5,2.0);
+		dividenumbers(4,0);
+		
+		
+		
 		
 	}
 	
