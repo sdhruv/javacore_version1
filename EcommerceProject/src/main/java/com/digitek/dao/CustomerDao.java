bb63abc968a16f6281a4d040d483a0fbfcbb6e90
sdhruv <dhruvshardul@gmail.com>
2016-06-10 18:21:08 -0500

    Ecommerce Project update
    Added two new methods (1) Delete Customer
    (2) Update Customer

--- EcommerceProject/src/main/java/com/digitek/dao/CustomerDao.java
+++ EcommerceProject/src/main/java/com/digitek/dao/CustomerDao.java
@@ -2,6 +2,7 @@ package com.digitek.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -27,10 +28,23 @@ public class CustomerDao {
 	
 
 	public static void deleteCustomer() {
+		try{
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce","root", "Shar1308");
+		Statement st = con.createStatement();
+		//i am deleting the row where the id is "6"
+		String query = "delete from Customer where Id = ?";
+		PreparedStatement preparedStmt = con.prepareStatement(query);
+		preparedStmt.setInt(1, 6);
+		preparedStmt.execute();
+		con.close();
+					
+		}
+		catch(Exception e) {
+			System.err.println("Got an Exception!");
+			System.err.println(e.getMessage());
+		}
 		
-	}
-	
-	public static void updateCustomer() {
 		
 	}
 	
@@ -38,10 +52,29 @@ public class CustomerDao {
 		
 	}
 	
+	public static void updateCustomer() {
+
+		try{
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce","root", "Shar1308");
+		Statement st = con.createStatement();
+		st.executeUpdate("insert into Customer "+ "values (06, 'Saumil', 'Mehta', 'saumil@outlook.com', 'sau2355', curdate(), 6)");
+		st.executeUpdate("insert into Customer "+ "values (07, 'Narendra', 'Patel', 'NCP@yahoo.com', 'keya2706', curdate(), 7)");
+		st.executeUpdate("insert into Customer "+ "values (08, 'Mayurika', 'Ghelani', 'mayur123@yahoo.com', 'may1308', curdate(), 8)");
+		con.close();
+		} catch (Exception e){
+		
+		}
+				
+		System.out.println("Got an exception! ");
+									
+	}
+	
 	
 	public static void main(String[] args) throws ClassNotFoundException, SQLException{ 	
           readCustomer();
-          
+          //updateCustomer();
+          //deleteCustomer();
           
 		}
 		
